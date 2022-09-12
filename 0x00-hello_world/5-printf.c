8e39353aa95aa919e7cbb20de12c9085a6f1ca46
akalewoled <fikerbdu@gmail.com>
2022-09-12 12:31:48 -0700

    äbc '' '
    ''

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,8 +6,8 @@
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	printf( "with proper grammar, but the outcome is a piece of art,\n");
+	return( 0);
 /*do what u gat to do dont be emotional
  * yead you do weather u feeldoing it 
  * */
