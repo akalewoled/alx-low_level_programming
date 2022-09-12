36232e2aa622012332f620f2c31300c5670bd42f
akalewoled <fikerbdu@gmail.com>
2022-09-12 12:55:03 -0700

    abcd

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,10 +16,10 @@ int main(void)
 	float f;
 
 	printf("size of char : %ld byte(s)\n",sizeof(c));
-	printf("size of an int:%ld byte(s)\n"sizeof(i));
+	printf("size of an int:%ld byte(s)\n",sizeof(i));
 	printf("size of a long int: %ld byte(s)\n",sizeof(li));
-	printf("size of a long long int: %ld byte(s)\n",sizeof(lli))
-	printf("size of a float: %ld byte(s)\n",sizeof(f))
+	printf("size of a long long int: %ld byte(s)\n",sizeof(lli));
+	printf("size of a float: %ld byte(s)\n",sizeof(f));
 	return(0);
 /*abcdefg
  * new
