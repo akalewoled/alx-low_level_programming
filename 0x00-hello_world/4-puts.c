62d833898fd028b726ef239c665c6f826fc27eda
akalewoled <fikerbdu@gmail.com>
2022-09-13 02:47:03 -0700

    bb

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
-/*the commt 
- * using betty dcumntaion style
+/*
+ * * using betty dcumntaion style
  * Discription :using the main funuction
- *Return: 0
+ * Return: 0
  */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
