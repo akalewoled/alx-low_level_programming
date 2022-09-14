055de386602bd78b6c7ad61f7891035c52af2145
akalewoled <fikerbdu@gmail.com>
2022-09-14 01:55:59 -0700

    abcd

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -20,12 +20,21 @@ void jack_bauer(void)
 		while (minutes <= 59)
 		{
 			mins_remainder = minutes % 10;
-			hours_remainder = hours % 10;													_putchar(hours / 10 + '0');
+			int i;
+
+			i = hours % 10;
+			hours_remainder = i;
+			_putchar(hours / 10 + '0');
 			_putchar(hours_remainder + '0');
 			_putchar(':');
-			_putchar(minutes / 10 + '0');
+			int a;
+
+			a = minutes / 10;
+
+			_putchar(a + '0');
 			_putchar(mins_remainder + '0');
-			minutes++;															_putchar('\n');
+			minutes = minutes + 1;
+			_putchar('\n');
 		}
 		hours++;
 		minutes = 0;
