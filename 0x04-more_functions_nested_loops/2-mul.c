3e38cd0fc84f49ae5caebb4ffef2d992c256c110
akalewoled <fikerbdu@gmail.com>
2022-09-15 14:06:25 -0700

    abcd

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+/**
+  * mul - multiplies two integers
+ *
+    *
+ *
+     * @a: first input
+ *
+      * @b: second input
+      * Return: 0 
+ */
+int mul(int a, int b)
+
+{
+
+		return (a * b);
+
+}
