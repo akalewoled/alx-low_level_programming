4b0f06f4f9b3edc794a2507fd1aff74116bd1574
akalewoled <fikerbdu@gmail.com>
2022-09-19 12:52:36 -0700

    abcd

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,9 @@
 /**
-* reset_to_98 - takes a pointer to an int parameter    and updates the value it points to 98
- *    * @n: int paramete
+* reset_to_98 - takes a pointer to an int parameter    and updates the va
+* @n: int paramete
      * Return: Nothing
       */
-
-
-
- void reset_to_98(int *n)
+void reset_to_98(int *n)
 
 {
 
