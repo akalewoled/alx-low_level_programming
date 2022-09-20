5ff5c5b6c202a2bb48ed419c37944ae4b12a9a9c
akalewoled <fikerbdu@gmail.com>
2022-09-20 08:08:35 -0700

    abcd

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,16 +10,25 @@ void puts_half(char *str)
 {
 	int l, n;
 
-	for (l = 0; str[l] != '\0'; ++l)
-		if (l % 2 == 0)
-		{
-			for (n = l / 2; str[n] != '\0'; ++n)
-				_putchar(str[n]);
-		}
-		else
+	l = _strlen(str);
+	if (l % 2 == 0)
+	{
+		for (n = l / 2; str[n] != '\0'; ++n)
+			_putchar(str[n]);
+	}
+	else
 		{
 			for (n = ((l - 1) / 2) + 1; str[n] != '\0'; ++n)
 				_putchar(str[n]);
 		}
 	_putchar('\n');
+}
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+	return (len);
 }
