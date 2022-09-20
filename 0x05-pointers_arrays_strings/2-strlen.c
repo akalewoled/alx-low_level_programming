8c2a48a0f44b00b1cfbae906cc6ccfefba866fa9
akalewoled <fikerbdu@gmail.com>
2022-09-19 23:16:17 -0700

    abcd

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -9,10 +9,11 @@
 int _strlen(char *s)
 
 {
-		int counter;
+	int counter;
 
-			for (counter = 0; *s != '\0'; ++s)
-				++counter;
-				return (counter);
-
+	for (counter = 0; *s != '\0'; ++s)
+	{
+		++counter;
+	}
+	return (counter);
 }
