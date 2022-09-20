db79d34bf2f405e58b75cd156837b63909ee2f76
akalewoled <fikerbdu@gmail.com>
2022-09-20 05:45:21 -0700

    last edit

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,4 @@
 #include"main.h"
-
-
 /**
   * puts2 - prints every character of a strin
   * @str: string parameter in
@@ -14,7 +12,7 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; ++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (i % 2 == 0)
 			_putchar(str[i]);
