8cff9c31ca0f41af006df62e20c474e654eb67ca
akalewoled <fikerbdu@gmail.com>
2022-09-23 12:44:20 -0700

    abcd

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,21 +1,17 @@
-#include "holberton.h"
-#include <stdio.h>
+#include "main.h"
 /**
- * main - check the code for ALX School students.
- * Return: Always 0.
+   *_strcat - concatenates  the string pointed to by @src to
+     *@dest: String that will be appended
+      *@src: String to be concatenated upon
+       * Return: returns poiner to @dest
  */
 
-int main(void)
+char *_strcat(char *dest, char *src)
 {
-	char s1[98] = "Hello ";
-	char s2[] = "World!\n";
-	char *ptr;
-
-	printf("%s\n", s1);
-	printf("%s", s2);
-	ptr = _strcat(s1, s2);
-	printf("%s", s1);
-	printf("%s", s2);
-	printf("%s", ptr);
-	return (0);
+	int index = 0, dest_len = 0;
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+	return (dest);
 }
