d87f438ad6afabd70f61c5c52fee91143cb513fb
akalewoled <fikerbdu@gmail.com>
2022-09-23 13:21:45 -0700

    abcd

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,29 +1,21 @@
-#include "holberton.h"
-#include <stdio.h>
-
-
+#include "main.h"
 
 /**
- * main - check the code for ALX School students.
- *    Return: Always 0.
-  */
+ * _strncat - concatenate two strings but add inputted number of bytes
+ * @dest: string to be appended upon
+* @src: string to be completed at end of des
+* @n:integer parameter to compare index to
+ *Return: returns new concatenated string
+*/
 
-int main(void)
-
+char *_strncat(char *dest, char *src, int n)
 {
-	char s1[98] = "Hello ";
-	char s2[] = "World!\n";
-	char *ptr;
+	int index = 0, dest_len = 0;
 
-	printf("%s\n", s1);
-	printf("%s", s2);
-	ptr = _strncat(s1, s2, 1);
-	printf("%s\n", s1);
-	printf("%s", s2);
-	printf("%s\n", ptr);
-	ptr = _strncat(s1, s2, 1024);
-	printf("%s", s1);
-	printf("%s", s2);
-	printf("%s", ptr);
-	return (0);
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+	return (dest);
+
 }
--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,21 +1,17 @@
-/**
- *   _strncpy - A function that copies a string
- *  * @dest: pointer to destination input buffer
- *    @src: pointer to source input buff
- *     @n: bytes of @sr
- *       Return: @dest
- */
-char *_strncpy(char *dest, char *src, int n)
-{
-		int i;
+#include "main.h"
 
-		for (i = 0; i < n && src[i] != '\0'; i++)
-			dest[i] = src[i];
-		while (i < n)
-		{
-			dest[i] = '\0';
-			i++;
-			return (dest);
-				}
-		return (dest);
-				}
+/**  *_strncat - concatenate two strings but add inputted number of bytes
+   *@dest: string to be appended upon
+   *@src: string to be completed at end of dest
+      *@n:integer parameter to compare index to
+      *Return: returns new concatenated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int index = 0, dest_len = 0;
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+	return (dest);
+}
