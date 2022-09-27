5c8adc85f7dff2c092d049103d6c1cc5c0803adc
akalewoled <fikerbdu@gmail.com>
2022-09-27 13:39:32 -0700

    no 5 and 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_chessboard - display 8 interval
+ * @a :array
+ * Return :nul
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+	while (i < 64)
+	{
+		if (i % 8 == 0 && i != 0)
+		{
+			j = i;
+			_putchar('\n');
+		}
+		_putchar(a[i / 8][i - j]);
+		i++;
+	}
+	_putchar('\n');
+}
