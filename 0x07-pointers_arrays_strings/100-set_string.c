b9205048a9c1bd236094623d2d4364b82a9fb88b
akalewoled <fikerbdu@gmail.com>
2022-09-27 14:59:59 -0700

    last

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main."
+/**
+ * set_string - Sets the value of a pointer to a char.
+ * @s: The pointer.
+ * @to: The char.
+ * Return :abcd
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
