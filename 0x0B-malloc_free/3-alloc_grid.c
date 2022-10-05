0322112865ebbf5952f5e574988a34473cb1066a
akalewoled <fikerbdu@gmail.com>
2022-10-05 13:45:44 -0700

    bb

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,17 +17,7 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = malloc(width * sizeof(int));
-		if (grid[i] == NULL)
-		{
-			while (i >= 0)
-			{
-				free(grid[i]);
-				i--;
-			}
-				free(grid);
-				return (NULL);
-		}
-			for (j = 0; j < width; j++)
+		for (j = 0; j < width; j++)
 				grid[i][j] = 0;
 	}
 		return (grid);
