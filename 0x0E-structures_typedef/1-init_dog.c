2f9de8550d7efddc549a0f692a026f3f123f7653
akalewoled <fikerbdu@gmail.com>
2022-10-10 13:26:55 -0700

    elele

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * init_dog - ssghn
+ * @name: the aem
+ * @age :age
+ * @owner: the new owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		/*d->name = name;*/
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
+	}
+}
