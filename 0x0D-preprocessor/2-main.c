c322f70e1b38bdf45ea4ab4037cb5adf89b058fb
akalewoled <fikerbdu@gmail.com>
2022-10-11 11:52:59 -0700

    abcd

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry poin
+ * Description: prints the name of the file it wa
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
