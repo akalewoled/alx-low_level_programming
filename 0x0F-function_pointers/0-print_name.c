d00e8ce7f3d3e3c0cdd798cb68d48c7a7241833c
akalewoled <fikerbdu@gmail.com>
2022-10-13 08:34:50 -0700

    bcd

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,7 +2,7 @@
  * print_name - a funuctoin that prints name
  * @name :the name to be prnted
  * @f :i dont know whyi nrted trhis
- * Return: nothing 
+ * Return: nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
