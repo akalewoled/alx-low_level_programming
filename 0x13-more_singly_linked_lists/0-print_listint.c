061db12bce6a1bd31d2c394de1144607033cd26c
akalewoled <fikerbdu@gmail.com>
2022-10-25 04:24:56 -0700

    correction on 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,16 +6,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t node_count = 1;
+	size_t node_count = 0;
 
-	if (h == NULL)
-		return (0);
 	while (h->next != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 		node_count++;
 	}
-	printf("%d\n", h->n);
 	return (node_count);
 }
