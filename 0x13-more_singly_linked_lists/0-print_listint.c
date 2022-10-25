304644db6e905a9ef4ddce59bd181ddb6809ffe7
akalewoled <fikerbdu@gmail.com>
2022-10-25 11:13:59 -0700

    betty style

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,7 +3,8 @@
 /**
   * print_listint - a function that prints all elements of a list
     * @h: pointer to first node
-    * Return: the number of nodes   */
+    * Return: the number of nodes
+    */
 
 size_t print_listint(const listint_t *h)
 {
